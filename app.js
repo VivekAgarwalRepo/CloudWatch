f6c8e5ce8260ce711e7a54b0e827573922412af6
nayangoelsjsu <nayan.goel@sjsu.edu>
2016-11-18 16:40:21 -0800

    4th commit
    4th commit

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+  app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 app.use(session({
 	secret: 'cmpe273_teststring',
 	resave: false,  //don't save session if unmodified
